ad481ef6fb5a564c14388b462129a51510410eef
Travis Sisti <travhimself@gmail.com>
2012-07-09 21:00:07 -0600

    serving other static files

--- server.js
+++ server.js
@@ -7,21 +7,33 @@ var path = require('path');
 // import util for accessing the file system
 var fs = require('fs');
 
+extensions = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.less': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.gif': 'image/gif',
+    '.jpg': 'image/jpeg',
+    '.woff': 'font/woff'
+};
+
 // create the server
 http.createServer(function (req, res) {
 
     // look for filename in the url, or if none exists, default to index.html
     var filename = path.basename(req.url) || 'index.html';
     var ext = path.extname(filename);
+    var dir = path.dirname(req.url).substring(1);
     var localpath = __dirname + "/";
 
-    if (ext == '.html') {
-        localpath += filename;
+    if (extensions[ext]) { // this will either contain a mimetype or will be undefined, in which case it will jump out
+        localpath += (dir ? dir + '/' : '') + filename;
 
         // confirm file exists and serve, or return 404
         fs.exists(localpath, function (exists) {
             if (exists) {
-                getFile(localpath, res);
+                getFile(localpath, extensions[ext], res);
             } else {
                 res.writeHead(404);
                 res.end();
@@ -31,10 +43,14 @@ http.createServer(function (req, res) {
 
 }).listen(8000, '127.0.0.1');
 
-function getFile(localpath, res) {
+function getFile(localpath, mimetype, res) {
     // read and return the file, or 500
     fs.readFile(localpath, function(err, contents) {
         if (!err) {
+            res.writeHead(200, {
+                'Content-Type': mimetype,
+                'Content-Length': contents.length
+            });
             res.end(contents);
         } else {
             res.writeHead(500);
