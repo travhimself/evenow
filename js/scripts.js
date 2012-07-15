9f66bd9018db3a30985b7a179ed4d1eb16dc9df2
Travis Sisti <travhimself@gmail.com>
2012-07-14 18:28:10 -0600

    cleaning up node modules; troubleshooting socket.io symlink issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,55 @@
 $(document).ready( function() {
 
     // TIME
+
+    // set up socket.io connection
+    var socket = io.connect("http://localhost:1111");
+
+    // listen for updatetime event from the server
+    socket.on("updatetime", function (data) {
+        alert(data.time);
+    });
+
+    // ask node server for an updated time every x seconds
+    var askfortimeupdate = socket.emit("timeupdate");
+    setInterval(askfortimeupdate, 5000);
+
     // create a new moment
-    var now = moment.utc();
+    // var now = moment.utc();
 
     // update date and time
-    var timehours = $('.time .hours span');
-    var timeminutes = $('.time .minutes span');
-    var timeseconds = $('.time .seconds span');
-    var timemonth = $('.date .month span');
-    var timedate = $('.date .day span');
-    var timeyear = $('.date .year span');
-    var rendertime = function() {
-        now.add('s', 1);
-        timehours.text( now.format('HH') );
-        timeminutes.text( now.format('mm') );
-        timeseconds.text( now.format('ss') );
-        timemonth.text( now.format('MMM') );
-        timedate.text( now.format('DD') );
-        timeyear.text( now.format('YYYY') );
-    };
+    // var timehours = $('.time .hours span');
+    // var timeminutes = $('.time .minutes span');
+    // var timeseconds = $('.time .seconds span');
+    // var timemonth = $('.date .month span');
+    // var timedate = $('.date .day span');
+    // var timeyear = $('.date .year span');
+    // var rendertime = function() {
+    //     now.add('s', 1);
+    //     timehours.text( now.format('HH') );
+    //     timeminutes.text( now.format('mm') );
+    //     timeseconds.text( now.format('ss') );
+    //     timemonth.text( now.format('MMM') );
+    //     timedate.text( now.format('DD') );
+    //     timeyear.text( now.format('YYYY') );
+    // };
 
     // set initial time
-    rendertime();
+    // rendertime();
 
     // update time every 1 second
-    setInterval(rendertime, 1000);
+    // setInterval(rendertime, 1000);
+
+
+
+
+
+
+
+
+
+
+    //////////////////////////////////////////////////
 
     // dynamically scale type to fit containers
     // var scaletypeall = function() {
--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 var connect = require("connect");
+
+// create a socket.io server on port 1111
+var io = require("socket.io").listen(1111);
+
+// set up a node server on port 8000 with the connect module
 connect(connect.static(__dirname + "/")).listen(8000);
+
+// listen for websocket connections from a client
+io.sockets.on("connection", function (socket) {
+
+    // listen for timeupdate event from the client
+    socket.on("timeupdate", function (data) {
+        var newtime = "TIME GOES HERE";
+
+        // publish updatetime event with the new time
+        socket.emit("updatetime", {time: newtime});
+    });
+});
